3fd7d27550cb19ff7bb4434373740f1d332e019d
Serhii  Zadorozhnyi <zadorozhnyi.sergey2@gmail.com>
2022-10-22 19:30:33 +0900

    added binary search solution for sqrt(x)

--- sqrt(x)/index.ts
+++ sqrt(x)/index.ts
@@ -8,3 +8,25 @@ function mySqrt(x: number): number {
 
   return result;
 }
+
+// binary search solution
+function mySqrtBinary(x: number): number {
+  if (x < 2) return x;
+
+  let left = 0;
+  let right = x;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let square = mid * mid;
+
+    if (square === x) return mid;
+    if (square < x) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return right;
+}
